f3c79081932a8d70d15afcb982a043ad1a79c4c2
Mariano <marianoizarriaga6@gmail.com>
2025-11-19 01:24:05 -0300

    Se agregaron detalles en la LoginPage

--- apps/web/app/pages/LoginPage.tsx
+++ apps/web/app/pages/LoginPage.tsx
@@ -12,29 +12,70 @@ const LoginPage = () => {
         setFormValues({ ...formValues, [key]: val })
     }
 
+    /*
     const validateForm = () => {
-        loginSchema.parseAsync(formValues)
+        loginSchema.parseAsync(formValues) // tira error si los datos estan mal
             .then(console.log)
             .catch(console.log)
     }
+    */
 
-    return (<div>
+    const handleSubmit = async () => {
+        try {
+            await loginSchema.parseAsync(formValues)
+            console.log("El formato es valido", formValues)
+
+            /*
+            const res = await fetch("http://localhost:5173/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formValues)
+            })
+
+            if(!res.ok){
+                alert("Datos incorrectos")
+                return 
+            }
+
+            /*
+            const data = await res.json()
+            localStorage.setItem("token", data.token)
+            window.location.href = "/home"
+            */
+
+            //----PROBANDO QUE ANDE SIN EL BACKEND ----//
+
+            console.log("Simulando login SIN backend...");
+            localStorage.setItem("token", "token-falso-123");
+            //window.location.href = "/home";
+            return;
+
+        } catch (error) {
+            alert("Formato de datos incorrecto")
+            console.log("Error de validacion", error)
+        }
+        }
+    
+
+    return (<div className="max-w-sm mx-auto mt-10 p-5 border rounded shadow">
 
         <div aria-label="form-input">
             <label>
                 Email
             </label>
-            <input type="text" value={formValues.email} onChange={(v) => setValue('email', v.target.value)} />
+            <input type="email" value={formValues.email} onChange={(v) => setValue('email', v.target.value)}  className="border border-gray-300 rounded p-2 w-full mt-1 text-white"/>
 
         </div>
         <div aria-label="form-input">
             <label>
                 Password
             </label>
-            <input type="text" value={formValues.password} onChange={(v) => setValue('password', v.target.value)} />
+            <input type="password" value={formValues.password} onChange={(v) => setValue('password', v.target.value)} className="border border-gray-300 rounded p-2 w-full mt-1 text-white"/>
         </div>
 
-        <button onClick={() => validateForm()} >
+        <button onClick={ handleSubmit} className="bg-blue-600 text-white px-4 py-2 rounded mt-4" >
             Submit
         </button>
 
